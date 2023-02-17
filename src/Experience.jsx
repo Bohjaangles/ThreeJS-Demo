2d2cf68020f2b94a88aa8ae4a163967e9c341155
bohjaangles <bohjaangles@gmail.com>
2023-02-17 15:05:36 -0600

    edit scale of contact shadows to be larger to encapsulate the text

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -75,7 +75,7 @@ export default function Experience() {
         <ContactShadows 
             position-y={-1.4}
             opacity={0.4}
-            scale={5}
+            scale={6}
             blur={2.4}
         />
     </>
